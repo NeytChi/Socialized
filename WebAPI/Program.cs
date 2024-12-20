58886809c5bafbda7a9321e73c2d470aa1961190
MykytaChystyi <neyton61@gmail.com>
2024-12-20 15:46:21 +0200

    Підключення до WebApi Serilog

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,23 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using UseCases.Users;
 
 var builder = WebApplication.CreateBuilder(args);
-
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 // Add services to the container.
-
+builder.Services.AddSerilog();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IUsersManager, UsersManager>();
+builder.Services.AddTransient<IUserLoginManager, UserLoginManager>();
+builder.Services.AddTransient<IUserPasswordRecoveryManager, UserPasswordRecoveryManager>();
+builder.Services.AddSingleton(Log.Logger);
 
 builder.Services.AddAuthentication(option =>
 {

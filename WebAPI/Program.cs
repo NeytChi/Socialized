f5be7aad8a712af4a52eb109e182b1a89723ba6c
MykytaChystyi <neyton61@gmail.com>
2025-01-13 17:24:11 +0200

    Написав залежності для IgAccount, AutoPost, Admin & Package controllers

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using Core;
+using Core.FileControl;
 using Domain.Admins;
 using Domain.Appeals;
 using Domain.Appeals.Messages;
@@ -14,8 +16,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using UseCases.Admins;
+using UseCases.AutoPosts;
 using UseCases.AutoPosts.AutoPostFiles;
+using UseCases.InstagramAccounts;
+using UseCases.InstagramApi;
+using UseCases.InstagramApi.MockingApi;
+using UseCases.Packages;
 using UseCases.Users;
+using WebAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,37 +38,67 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 // Add services to the container.
 builder.Services.AddSerilog();
+builder.Services.AddSingleton(Log.Logger);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IAdminManager, AdminManager>();
-builder.Services.AddTransient<IAdminEmailManager, AdminEmailManager>();
+
+builder.Services.AddSingleton<IAdminManager, AdminManager>();
+builder.Services.AddSingleton<IJwtTokenManager, JwtTokenManager>();
+builder.Services.AddSingleton<IAdminRepository, AdminRepository>();
+builder.Services.AddSingleton<IAdminEmailManager, AdminEmailManager>();
+builder.Services.AddSingleton<ISmtpSender, SmtpSender>(); 
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+
+builder.Services.AddSingleton<IPackageManager, PackageManager>();
+builder.Services.AddTransient<IServiceAccessRepository, ServiceAccessRepository>();
+builder.Services.AddTransient<IPackageAccessRepository, PackageAccessRepository>();
+builder.Services.AddTransient<IDiscountRepository, DiscountRepository>();
+builder.Services.AddTransient<IForServerAccessCountingRepository, IGAccountRepository>();
+
+builder.Services.AddSingleton<IAutoPostManager, AutoPostManager>();
+builder.Services.AddTransient<IAutoPostRepository, AutoPostRepository>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IIGAccountRepository, IGAccountRepository>();
+builder.Services.AddSingleton<IAutoPostFileManager, AutoPostFileManager>();
+builder.Services.AddTransient<IFileManager, AwsUploader>();
+builder.Services.Configure<AwsSettings>(builder.Configuration.GetSection("AwsSettings"));
+builder.Services.AddTransient<IAutoPostFileRepository, AutoPostFileRepository>();
+builder.Services.AddTransient<IAutoPostFileSave, AutoPostFileSave>();
+
+
+
+builder.Services.AddSingleton<IIGAccountManager, CreateIGAccountManager>();
+builder.Services.AddTransient<ILoginApi, LoginApi>();
+builder.Services.AddTransient<IChallengeRequiredAccount, ChallengeRequiredAccount>();
+builder.Services.AddTransient<IGetChallengeRequireVerifyMethod, GetChallengeRequireVerifyMethod>();
+builder.Services.AddTransient<IVerifyCodeForChallengeRequire, VerifyCodeForChallengeRequire>();
+builder.Services.AddTransient<ILoginSessionManager, LoginSessionManager>();
+builder.Services.AddTransient<IRecoverySessionManager, RecoverySessionManager>();
+builder.Services.AddSingleton<ProfileCondition>();
+builder.Services.AddTransient<IGetChallengeRequireVerifyMethod, GetChallengeRequireVerifyMethod>();
+builder.Services.AddTransient<IGetStateData, GetStateData>();
+builder.Services.AddTransient<ISaveSessionManager, SaveSessionManager>();
+
+/// TODO: Write all dependencies for each controller
+
+builder.Services.AddTransient<IPackageAccessRepository, PackageAccessRepository>();
 builder.Services.AddTransient<IUsersManager, UsersManager>();
 builder.Services.AddTransient<IUserLoginManager, UserLoginManager>();
 builder.Services.AddTransient<IUserPasswordRecoveryManager, UserPasswordRecoveryManager>();
 
-// UserPasswordRecoveryManager
 
-
-builder.Services.AddTransient<IAdminRepository, AdminRepository>();
 builder.Services.AddTransient<IAppealFileRepository, AppealFileRepository>();
 builder.Services.AddTransient<IAppealMessageReplyRepository, AppealMessageReplyRepository>();
 builder.Services.AddTransient<IAppealMessageRepository, AppealMessageRepository>();
 builder.Services.AddTransient<IAppealRepository, AppealRepository>();
-builder.Services.AddTransient<IAutoPostFileRepository, AutoPostFileRepository>();
-builder.Services.AddTransient<IAutoPostRepository, AutoPostRepository>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IDiscountRepository, DiscountRepository>();
 builder.Services.AddTransient<IIGAccountRepository, IGAccountRepository>();
-builder.Services.AddTransient<IForServerAccessCountingRepository, IGAccountRepository>();
-builder.Services.AddTransient<IPackageAccessRepository, PackageAccessRepository>();
-builder.Services.AddTransient<IServiceAccessRepository, ServiceAccessRepository>();
 builder.Services.AddTransient<ITaskDataRepository, TaskDataRepository>();
 builder.Services.AddTransient<ITaskGsRepository, TaskGsRepository>();
 builder.Services.AddTransient<ITaskGettingSubscribesRepository, TaskGsRepository>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
-builder.Services.AddSingleton(Log.Logger);
 
 builder.Services.AddAuthentication(option =>
 {

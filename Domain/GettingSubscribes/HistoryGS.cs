0c6c5aac1ea1e2e22de20493f459f14be46a4996
MykytaChystyi <neyton61@gmail.com>
2025-01-21 15:54:03 +0200

    Видалив з Domain клас, котрі вже не актуальні

--- Domain/GettingSubscribes/HistoryGS.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.GettingSubscribes
-{
-    public partial class HistoryGS : BaseEntity
-    {
-        public long TaskId { get; set; }
-        public string Url  { get; set; }
-        public virtual TaskGS Task { get; set; }
-    }
-} 
--- Domain/Users/Profile.cs
+++ Domain/Users/Profile.cs
@@ -3,13 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Users
 {
+    [Table("Profiles")]
     public partial class Profile : BaseEntity
     {
         [ForeignKey("user")]
         public long UserId { get; set; }
         [MaxLength(100)]
-        public string CountryName { get; set; }
+        public required string CountryName { get; set; }
         public long TimeZone { get; set; }
-        public virtual User user { get; set; }
+        public virtual required User user { get; set; }
     }
 }
